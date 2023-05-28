084ab0754b1b420337778b3675d2f0dd46c15b65
pavan <pavankoneti1@gmail.com>
2023-05-28 20:56:09 +0530

    Revert "Initial commit"
    This reverts commit 9bef3b844d98e4352c88c70fb8ec90f042fe3f2d.

--- project/music/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Musics
-
-class MusicSerializer(ModelSerializer):
-    class Meta:
-        model = Musics
-        fields = "__all__"
